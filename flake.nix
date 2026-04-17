86c4961a3c31667e1e18ae3ef2d49264947d437f
Max Generowicz <mdcg-github@my-post-office.net>
2026-04-17 17:04:48 +0000

    Add ratpac as package to flake

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
 
         devShells.default = pkgs.mkShell {
           inputsFrom = [ self.packages.${system}.ratpac-two ];
+          packages = [ self.packages.${system}.ratpac-two ];
         };
       });
 }
